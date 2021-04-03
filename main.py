ec71dc577181b280fec8522e570c0d9d6b059181
Tyreek Hill <>
2021-04-03 23:39:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# #1. Create a greeting for your program.
+# print("This will generate a brand name")
+
+# #2. Ask the user for the city that they grew up in.
+# grew_up_in = input("\nwhat city did you grow up in?\n ")
+
+# #3. Ask the user for the name of a pet.
+# favorite_pet = input("\nwhat is the name of your favorite pet?\n ")
+
+# #4. Combine the name of their city and pet and show them their band name.
+# # print("The name of your company is, " + grew_up_in + favorite_pet )
+
+# #5. Make sure the input cursor shows on a new line 
+# #city
+# #"Newyork"
+
+# name = input("what is your name")
+# print(type(name))
+# len_name = len(name)
+# print(type(len_name))
+# # print("my name is" + name +"the lenght of my name is" + len_name) this is throwing type error because a str and integer are being concantenated
+# print("my name is " + name +" and the lenght of my name is" + str(len_name))
+# print(100+float(100.75))
+# print(str(100)+str(100))
+
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+first = two_digit_number [0]
+second = two_digit_number[1]
+first_int = int(first)
+second_int = int(second)
+print(first_int + second_int)
+
+
+
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇

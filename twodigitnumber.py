2d8114a1cb2be1a04c415221d8abc45b44fe669a
TyreekHill <>
2021-04-03 23:59:28 +0000

    twodigitnumber

---
+++ twodigitnumber.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Type a two digit number: ")
+first = two_digit_number [0]
+second = two_digit_number[1]
+first_int = int(first)
+second_int = int(second)
+print(first_int + second_int)
+

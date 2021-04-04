d026c3aeea8aba5463b1db24d36f77eb77979874
TyreekHill <>
2021-04-04 00:19:49 +0000

    pemdas.py

---
+++ pemdas.py
@@ -0,0 +1,13 @@
+#P -Parenthesis ()
+#E-Exponent **
+#M-Multiply *  #D- Division /.      (L-R)
+#A-Addition + #S-Subtracttion -           (L-R)
+
+print(2/2) # 1.0
+print(type(2/2))
+print(2**2) # 4
+print(2**3) # 8
+print(2**2*2) # 8
+print(3*3+3/3-3) # 7.0
+print(3*(3+3)/3-3) # 3.0
+print(3*(3+3)/3-3**3) # -21.0

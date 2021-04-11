35b9d3580e151c28ec80e341701024bcf0c70486
TyreekHill <>
2021-04-11 00:27:40 +0000

    oddoreven

---
+++ conditionalblock/oddoreven/oddoreven.py
@@ -0,0 +1,6 @@
+print("I will tell you if a number is even or odd")
+number = int(input("Type your number:\t "))
+if(number % 2 == 0):
+  print(f"{number} is even")
+else: 
+  print(f"{number} is odd")

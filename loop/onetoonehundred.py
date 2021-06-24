d3836cc375eaa5dc276d4932d74fefe8b024f506
TyreekHill <>
2021-06-24 14:55:18 +0000

    loop

---
+++ loop/onetoonehundred.py
@@ -0,0 +1,39 @@
+# print range
+# num_range= (100)
+# numrange = int(num_range)
+# print(type(numrange))
+
+# for n in range(1,101):
+#   print(n)
+
+# print sum of range
+
+
+num_range= (100)
+numrange = int(num_range)
+print(type(numrange))
+n = 1
+
+sum = 0
+for n in range(1,1001):
+  sum = sum + n
+print(sum)
+# print add even numbers of range
+num_range= (100)
+numrange = int(num_range)
+print(type(numrange))
+n = 1
+
+sum = 0
+for n in range(0,101,2):
+  sum = sum + n
+print(sum)
+
+
+# student_scores = input("Input a list of student scores ").split(",")
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+# print(student_scores)
+
+
+# n * n + 1/2
